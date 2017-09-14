40a12fe37afc016f7d5f7d14ee2c38d22447e89b
NgocB <ngoc.bach@eastagile.com>
2017-09-14 15:50:58 +0700

    Remove name index in songs. Commit time: 15:14:14 14/09/17 in HNNB.local

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914033333) do
+ActiveRecord::Schema.define(version: 20170914084936) do
 
   create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "attachmentable_type"
@@ -99,7 +99,6 @@ ActiveRecord::Schema.define(version: 20170914033333) do
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
-    t.index ["name"], name: "index_songs_on_name", unique: true, using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
