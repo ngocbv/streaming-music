441e967e77e9f7eed4f9c84a39ad75f57770db2e
NgocB <ngoc.bach@eastagile.com>
2017-09-14 15:17:07 +0700

    Add name index to songs and first_name, last_name index to users. Commit time: 15:14:14 14/09/17 in HNNB.local

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701084140) do
+ActiveRecord::Schema.define(version: 20170914033333) do
 
   create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "attachmentable_type"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 20170701084140) do
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.index ["name"], name: "index_songs_on_name", unique: true, using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -118,7 +119,10 @@ ActiveRecord::Schema.define(version: 20170701084140) do
     t.string   "image_url"
     t.string   "name"
     t.integer  "role"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["first_name", "last_name"], name: "index_users_on_first_name_and_last_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
